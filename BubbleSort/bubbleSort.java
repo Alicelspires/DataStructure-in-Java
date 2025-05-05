45652100f6d78cf9ff40b56f3d12a1129df4fae0
Alice Silva <alicelspires@gmail.com>
2025-05-05 00:40:37 -0300

    atualizando pasta bubble sort

--- BubbleSort/bubbleSort.java
+++ BubbleSort/bubbleSort.java
@@ -1,11 +1,22 @@
-public static void bubbleSort(int[] array) {
-    int n = array.length;
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (array[j] > array[j+1]) {
-                int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
+public class bubbleSort {
+    public static void main(String[] array) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.println("Array ordenado:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+    public static int bubbleSort(int[] array){
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {  // Corrigido: n-i-1 em vez de n-1-1
+                if (array[j] > array[j + 1]) {
+                    // Troca os elementos
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
             }
         }
     }

987bd01a01496d4b5899016fb7ef5a20f751675b
Alice Silva <alicelspires@gmail.com>
2025-05-20 19:37:42 -0300

    Update

--- Recursão/MDC.java
+++ Recursão/MDC.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 public class MDC{
 	public static void main(String[] args) {
 	    int mdc = mdc(250, 150);
-			System.out.println(mdc);
+		System.out.println(mdc);
 	}
-	public static int fib(int n, int v[]){
+	public static int mdc(int n, int v[]){
 	    if(n%m==0){
-				return m;
-			} else {
-				return mdc(m, n%m);
-			}
+			return m;
+		} else {
+			return mdc(m, n%m);
+		}
 	}
 }
  
